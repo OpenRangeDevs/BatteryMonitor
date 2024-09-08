0a6a3364fbdbe74d0d56171b226501676e5ac8a9
Roger Kondrat <misterhtmlcss@gmail.com>
2024-09-08 06:46:28 -0600

    Initial Commit

---
+++ BatteryMonitor/BatteryMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryMonitorApp.swift
+//  BatteryMonitor
+//
+//  Created by Roger Kondrat on 2024-09-08.
+//
+
+import SwiftUI
+
+@main
+struct BatteryMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
